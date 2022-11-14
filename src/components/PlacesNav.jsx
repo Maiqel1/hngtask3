911def05ed2a1f9e6655e159ea46c4a8e0ac40bc
maiqel <michaelolowe321@gmail.com>
2022-11-14 18:10:36 +0100

    feat: replaced navigation on places page with custom scroll bar

--- src/components/PlacesNav.jsx
+++ src/components/PlacesNav.jsx
@@ -4,7 +4,30 @@ function PlacesNav() {
 
   return (
     <div className="container mt-5 ms-0 ms-md-5 ">
-         <div className=" row">
+
+<div class="scrollmenu ms-0 ms-md-5 ps-0 ps-md-5">
+  <a href="ms-1 ms-md-5">Restaurant</a>
+  <a href="ms-1 ms-md-5">Cottage</a>
+  <a href="ms-1 ms-md-5">Castle</a>
+  <a href="ms-1 ms-md-5">Fantasy City</a>
+  <a href="ms-1 ms-md-5">Beach</a>
+  <a href="ms-1 ms-md-5">Cabins</a>
+  <a href="ms-1 ms-md-5">Off-grid</a>
+  <a href="ms-1 ms-md-5">Farm</a>
+    
+    <a className='d-inline ms-3 ms-md-4'>
+      <div className=' d-inline  location col-4 col-md-2 col-lg-2 p-2'>
+
+                    <p className='mt-2 ms-2 d-inline'>Location</p>
+                    <i className='mt-1 ms-3 d-inline'> <img src={Location} className='img-fluid' alt="" /> </i>
+              
+            </div>
+            
+    </a>
+
+</div>
+
+         {/* <div className=" row">
             <div className='d-flex justify-content-around  flex-wrap col-md-8 mt-3 ps-0 ps-md-5 ms-0  ms-md-5'>
             <p className='ms-2 ms-md-4'>Restaurant</p>
             <p className='ms-2 ms-md-4'>Cottage</p>
@@ -21,7 +44,7 @@ function PlacesNav() {
                     <i className='mt-1'> <img src={Location} className='img-fluid' alt="" /> </i>
               
             </div>
-        </div> 
+        </div>  */}
     </div>
   )
 }
